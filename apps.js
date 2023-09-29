af735977dc1f15b15dc1017cb528f2a97fca53be
Opeyemi <yemi.otu@outlook.com>
2023-09-28 19:56:39 -0600

    Javascript funtions with arguments

---
+++ apps.js
@@ -0,0 +1,17 @@
+function multiplybyfive (number1, number2){
+    return (number1 + number2) * 5
+}
+let resultsumtimesfive = multiplybyfive(7,10)
+    console.log(resultsumtimesfive);
+
+    
+let username = "missrachel2023";
+let long_string = username.length;
+    console.log(long_string);
+  if (long_string > 10) {
+    console.log("TRUE")
+  }else {
+    console.log("FALSE")
+  }
+
+                                     
